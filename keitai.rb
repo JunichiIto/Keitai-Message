58dae0000d0db8bff6e9a524177bf669eeb4c299
shinokada <sokada@canacad.ac.jp>
2014-04-12 21:45:00 +0900

    Added Algorithm

--- keitai.rb
+++ keitai.rb
@@ -1,4 +1,36 @@
 class Keitai
 
+  # initialize
+  # create a hash 
+  # letters = {}
+  # letters[:1] = ".,!?"
+  # letters[:2] = "abc"
+  # letters[:3] = "def"
+  # letters[:4] = "ghi"
+  # letters[:5] = "jkl"
+  # letters[:6] = "mno"
+  # letters[:7] = "pqrs"
+  # letters[:8] = "tuv"
+  # letters[:9] = "wxyz"
+  # letters[0] = ""
+
+
+  # trim the first 0s and split input by 0 
+  # find the first letter and count the length
+  # and put it in an array [[first-number, length]]
+  # e.g. [[2,1]], [[2,2]], [[2,5]], [[4,2],[3,2],[5,3],[6,3],[1,2],[1,5],[9,1],[6,3],[7,3],[5,3],[3,1],[1,3]]  
+
+  
+   
+  # find letter by using modulus operator for first part of each item of array item[], position = item[0] % letters[item[0]] and this should give the position of letter in the hash letters[item[0]][position-1]
+
+# Insert/add them to an array
+
+
+
+
+
+
+
 
 end
--- keitai_spec.rb
+++ keitai_spec.rb
@@ -5,7 +5,17 @@ require './keitai.rb'
 describe Keitai do
 
 
+  before do
+    input1 = 5
+    input2 = 20
+    input3 = 220
+    input4 = 222220
+    input5 = 44033055505550666011011111090666077705550301110
+    input6 = 000555555550000330000444000080000200004440000
+
+  end
 
 
+  specify{ expect(@intersection.check_size?). to eq false }
 
 end
