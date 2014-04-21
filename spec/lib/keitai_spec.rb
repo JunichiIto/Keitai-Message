1ad3f12223768935da3f9589c8ec9676ca5a1c56
Junichi Ito <jit@sonicgarden.jp>
2014-04-22 04:54:48 +0900

    Refactoring spec.

--- spec/lib/keitai_spec.rb
+++ spec/lib/keitai_spec.rb
@@ -1,25 +1,35 @@
 require 'spec_helper'
 
 describe Keitai do
-  before do
-    @input1 = '5'
-    @input2 = '20'
-    @input3 = '220'
-    @input4 = '222220'
-    @input5 = '44033055505550666011011111090666077705550301110'
-    @input6 = '000555555550000330000444000080000200004440000'
-    @keitai1 = Keitai.new(@input1)
-    @keitai2 = Keitai.new(@input2)
-    @keitai3 = Keitai.new(@input3)
-    @keitai4 = Keitai.new(@input4)
-    @keitai5 = Keitai.new(@input5)
-    @keitai6 = Keitai.new(@input6)
+  subject { Keitai.new(input).decipher }
+
+  context 'without 0' do
+    let(:input) { '5' }
+    it { should eq '' }
   end
 
-  specify { expect(@keitai1.decipher). to eq '' }
-  specify { expect(@keitai2.decipher). to eq 'a' }
-  specify { expect(@keitai3.decipher). to eq 'b' }
-  specify { expect(@keitai4.decipher). to eq 'b' }
-  specify { expect(@keitai5.decipher). to eq 'hello, world!' }
-  specify { expect(@keitai6.decipher). to eq 'keitai' }
+  context 'single input' do
+    let(:input) { '20' }
+    it { should eq 'a' }
+  end
+
+  context 'plural inputs' do
+    let(:input) { '220' }
+    it { should eq 'b' }
+  end
+
+  context 'with input loop' do
+    let(:input) { '222220' }
+    it { should eq 'b' }
+  end
+
+  context 'complicated input' do
+    let(:input) { '44033055505550666011011111090666077705550301110' }
+    it { should eq 'hello, world!' }
+  end
+
+  context 'start with 0 and repeat 0' do
+    let(:input) { '000555555550000330000444000080000200004440000' }
+    it { should eq 'keitai' }
+  end
 end
