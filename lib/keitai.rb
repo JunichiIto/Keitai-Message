271b766f31f262e8729170c57b7d1622c41c1035
Junichi Ito <jit@sonicgarden.jp>
2014-04-22 05:01:44 +0900

    Use class methods instead of instance methods.

--- lib/keitai.rb
+++ lib/keitai.rb
@@ -4,29 +4,15 @@
 class Keitai
   LETTERS = ['.,!? ', 'abc', 'def', 'ghi', 'jkl', 'mno', 'pqrs', 'tuv', 'wxyz'].freeze
 
-  ##
-  # allocate letters to array using array key
-  def initialize(input)
-    @input = input
-  end
-
   def self.decipher(input)
-    self.new(input).decipher
-  end
-
-  ##
-  # Convert strings to letter
-  def decipher
-    @input
+    input
       .scan(/([1-9]+)0/)
       .flatten
       .map{|item| covert_to_letter(item) }
       .join
   end
 
-  private
-
-  def covert_to_letter(item)
+  def self.covert_to_letter(item)
     letter_index = item[0].to_i - 1
     position = item.length % LETTERS[letter_index].length - 1
     LETTERS[letter_index][position]
