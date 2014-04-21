6f45f021f77182405d96f0203e43272e6dccdaee
Junichi Ito <jit@sonicgarden.jp>
2014-04-22 04:32:42 +0900

    Remove guard condition.

--- lib/keitai.rb
+++ lib/keitai.rb
@@ -16,12 +16,14 @@ class Keitai
   ##
   # Convert strings to letter
   def decipher
-    normalized_input.split('0').map{|item| covert_to_letter(item) }.join
+    normalized_input
+      .split('0')
+      .reject(&:empty?)
+      .map{|item| covert_to_letter(item) }
+      .join
   end
 
   def covert_to_letter(item)
-    return '' if item.empty?
-
     letter_index = item[0].to_i - 1
     position = item.length % @letters[letter_index].length - 1
     @letters[letter_index][position]
