8b13423ca1bc3c4bb80a7618f90c2c0c6c50aa40
Junichi Ito <jit@sonicgarden.jp>
2014-04-22 03:43:55 +0900

    Use attr_reader.

--- lib/keitai.rb
+++ lib/keitai.rb
@@ -2,6 +2,8 @@
 # @param input [String]
 # @return [String] converted letters
 class Keitai
+  attr_reader :input
+
   ##
   # allocate letters to array using array key
   def initialize(input)
@@ -20,16 +22,10 @@ class Keitai
   end
 
   ##
-  # Returen initialized input
-  def input
-    @input
-  end
-
-  ##
   # Convert strings to letter
   def decipher
-    if @input
-      main_arr = @input.split('0').map do |item|
+    if input
+      main_arr = input.split('0').map do |item|
         number = item[0].to_i
         item_length = item.length
         letter_length = @letters[number].length
